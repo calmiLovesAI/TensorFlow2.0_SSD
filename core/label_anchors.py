3e8bd6313a9108062d6afa7451d3f759367f9f4f
calmisential <xinyu_std@163.com>
2019-09-10 17:46:27 +0800

    Upload label_anchors.py

--- core/label_anchors.py
+++ core/label_anchors.py
@@ -15,13 +15,21 @@ class LabelAnchors():
         self.iou = IoU.IoU()
         self.anchors = anchors
         self.labels = labels
+        self.batch_size = anchors.shape[0]
         self.num_anchors = anchors.shape[1]
         self.num_true_boxes = labels.shape[1]
 
 
-    def generate_iou_matrix(self):
+    def generate_iou_array(self):
+        iou_list = []
         for i in range(self.num_anchors):
+            temp_list = []
             for j in range(self.num_true_boxes):
                 iou_ij = self.iou.calculate_iou(n_dims_coord_pred=self.anchors[:, i, :],
                                                 n_dims_coord_true=self.labels[:, j, -4:])
+                temp_list.append(iou_ij)
+            iou_list.append(temp_list)
+
+        return np.array(iou_list)
+
 
