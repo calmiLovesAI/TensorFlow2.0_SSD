caefc52ed74ad9447332e77b7b07d43a9832d597
calmisential <xinyu_std@163.com>
2019-09-16 17:47:15 +0800

    Update

--- configuration.py
+++ configuration.py
@@ -7,3 +7,7 @@ IAMGE_WIDTH = 256
 # When the iou value of the anchor and the real box is less than the IoU_threshold,
 # the anchor is divided into negative classes, otherwise positive.
 IoU_threshold = 0.6
+
+# focal loss
+alpha = 0.25
+gamma = 2
--- core/loss.py
+++ core/loss.py
@@ -1,3 +1,5 @@
 import tensorflow as tf
+from configuration import alpha, gamma
 
-
+def focal_loss(y_pred, y_true):
+    pass
