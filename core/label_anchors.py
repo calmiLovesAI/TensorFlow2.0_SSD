c10ee8d29f4ff8703426766db9a461a4ece9ab6b
calmisential <xinyu_std@163.com>
2019-09-16 16:13:17 +0800

    Update label_anchors.py

--- core/label_anchors.py
+++ core/label_anchors.py
@@ -1,51 +1,87 @@
 import tensorflow as tf
-import parameters
+from configuration import IoU_threshold
 import numpy as np
 from utils import IoU
 
 class LabelAnchors():
     def __init__(self, anchors, labels, class_preds):
-        # anchors : [batch_size, total number pf anchors, 4]
+        # anchors : [batch_size, total number of anchors, 4]
         # labels shape :
         # [batch_size, num_of_objects_per_image(-1 for no object), 5(class + bbox coords)]
         # class_preds : [batch_size, total number pf anchors, num_classes + 1]
         super(LabelAnchors, self).__init__()
-        parameter = parameters.Parse()
-        self.iou_threshold = parameter.get_iou_threshold()
+        self.iou_threshold = IoU_threshold
         self.iou = IoU.IoU()
         self.anchors = anchors
         self.labels = labels
-        self.batch_size = anchors.shape[0]
+        self.batch_size = labels.shape[0]
         self.num_anchors = anchors.shape[1]
         self.num_true_boxes = labels.shape[1]
 
 
-    def __generate_iou_array(self):
+    def __generate_iou_array(self, batch):
         iou_list = []
         for i in range(self.num_anchors):
             temp_list = []
             for j in range(self.num_true_boxes):
-                iou_ij = self.iou.calculate_iou(n_dims_coord_pred=self.anchors[:, i, :],
-                                                n_dims_coord_true=self.labels[:, j, -4:])
+                iou_ij = self.iou.calculate_iou(coord_pred=self.anchors[0, i, :],
+                                                coord_true=self.labels[batch, j, -4:])
                 temp_list.append(iou_ij)
             iou_list.append(temp_list)
 
-        return np.array(iou_list)  # shape: [self.num_anchors, self.num_true_boxes, batch_size, 1]
+        return np.array(iou_list)  # shape: [self.num_anchors, self.num_true_boxes]
 
+
+    def get_results(self):
+        # iou_array_list = []
+        for b in range(self.batch_size):
+            anchor_index_list = []
+            # For every input image
+            iou_array = self.__generate_iou_array(batch=b)
+            iou_array_copy = iou_array
+            while(iou_array.shape[1]):
+                # First find the maximum value of all elements of the matrix, and then
+                # according to the row and column where the maximum value is located,
+                # we can obtain the corresponding anchor and true box match.
+                match_list = self.__get_the_max_value_of_matix(iou_array)
+                anchor_index = match_list[0]
+                anchor_index_list.append(anchor_index)
+                true_box_index = match_list[1]
+                # Calculate the offset value.
+                offset_array = self.__get_offset(pred_array=self.anchors[0, anchor_index, :],
+                                                 true_array=self.labels[b, true_box_index, -4:])
+                iou_array = self.__throw_row_and_col(iou_array, anchor_index, true_box_index)
+            # All true boxes have been allocated, then we allocate the remaining anchors.
+            for i in range(self.num_anchors):
+                if i not in anchor_index_list:
+                    # j : true box index
+                    j = self.__get_max_value_of_one_row(i, iou_array_copy)
+                    # calculate iou
+                    iou_value = self.iou.calculate_iou(coord_pred=self.anchors[0, i, :],
+                                                       coord_true=self.labels[b, j, -4:])
+                    # The anchor is evaluated as a positive sample only if iou_value is greater than iou_threshold.
+                    if iou_value > self.iou_threshold:
+                        # calculate offset
+                        offset_array = self.__get_offset(pred_array=self.anchors[0, i, :],
+                                                         true_array=self.labels[b, j, -4:])
+
+
+
+
+            # iou_array_list.append(iou_array)
+
+
+    def __get_max_value_of_one_row(self, index, matrix):
+        max_index = np.argmax(matrix, axis=1)
+        return max_index[index]
+
+
+    # Return the row and column where the maximum value is located.
     def __get_the_max_value_of_matix(self, matrix):
         max_index = np.argmax(matrix)
         i = max_index % matrix.shape[0]
         j = max_index % matrix.shape[1]
         return [i, j]
-
-    def __get_the_max_value_of_iou_array(self, iou_array):
-        # iou_array = iou_array.reshape((self.num_anchors, self.num_true_boxes, -1))
-        max_value_list = []
-        for index in range(iou_array.shape[2]):
-            max_list = self.__get_the_max_value_of_matix(iou_array[:, :, index, 1])
-            max_value_list.append(max_list)
-
-        return max_value_list
 
     def __get_offset(self, pred_array, true_array):
         offset_list = []
@@ -55,6 +91,7 @@ class LabelAnchors():
         offset_array = np.array(offset_list)
 
         return offset_array
+
 
     def __throw_row_and_col(self, matrix, i, j):
         # Throw away a row and a column of the matrix.
@@ -67,11 +104,5 @@ class LabelAnchors():
 
         return np.concatenate((np.concatenate((part_1, part_2), axis=1), np.concatenate((part_3, part_4), axis=1)), axis=0)
 
-    def get_results(self):
-        # max_values : [[i1, j1], [i2, j2], ..., [in, jn]]
-        max_values = self.__get_the_max_value_of_iou_array(iou_array=self.__generate_iou_array())
-
-        for item in max_values:
-            # for each picture entered
 
 
