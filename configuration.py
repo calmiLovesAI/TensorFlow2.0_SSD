59c72412aaf4fdf54466f74f6d6582a4e225d122
calmisential <xinyu_std@163.com>
2019-09-18 13:32:35 +0800

    Add object class dict of pascal voc

--- configuration.py
+++ configuration.py
@@ -16,3 +16,9 @@ gamma = 2
 
 # dataset
 PASCAL_VOC_DIR = "./dataset/VOCdevkit/VOC2012/"
+# The 20 object classes of PASCAL VOC
+OBJECT_CLASSES = {"person": 1, "bird": 2, "cat": 3, "cow": 4, "dog": 5,
+                  "horse": 6, "sheep": 7, "aeroplane": 8, "bicycle": 9,
+                  "boat": 10, "bus": 11, "car": 12, "motorbike": 13,
+                  "train": 14, "bottle": 15, "chair": 16, "diningtable": 17,
+                  "pottedplant": 18, "sofa": 19, "tvmonitor": 20}
--- parse_pascal_voc.py
+++ parse_pascal_voc.py
@@ -1,5 +1,5 @@
 from xml.dom.minidom import parse
-from configuration import PASCAL_VOC_DIR
+from configuration import PASCAL_VOC_DIR, OBJECT_CLASSES
 import os
 
 # parse one xml file
@@ -8,14 +8,12 @@ def parse_xml(xml):
     DOMTree = parse(xml)
     annotation = DOMTree.documentElement
     image_name = annotation.getElementsByTagName("filename")[0].childNodes[0].data
-    # print("image_name : ", image_name.childNodes[0].data)
 
     obj = annotation.getElementsByTagName("object")
     for o in obj:
         o_list = []
         obj_name = o.getElementsByTagName("name")[0].childNodes[0].data
-        o_list.append(obj_name)
-        # print("object name : ", obj_name.childNodes[0].data)
+        o_list.append(OBJECT_CLASSES[obj_name])
         bndbox = o.getElementsByTagName("bndbox")
         for box in bndbox:
             xmin = box.getElementsByTagName("xmin")[0].childNodes[0].data
@@ -27,18 +25,14 @@ def parse_xml(xml):
             o_list.append(xmax)
             o_list.append(ymax)
             break
-            # print("xmin = {}".format(xmin))
-            # print("xmax = {}".format(xmax))
-            # print("ymin = {}".format(ymin))
-            # print("ymax = {}".format(ymax))
         obj_and_box_list.append(o_list)
     return image_name, obj_and_box_list
 
 
 if __name__ == '__main__':
     all_xml_dir = PASCAL_VOC_DIR + "Annotations"
-    # for item in os.listdir(all_xml_dir):
-    #     item_dir = os.path.join(all_xml_dir, item)
-    image_name, boxes_list = parse_xml(xml=os.path.join(all_xml_dir, "2007_000032.xml"))
-    print(image_name)
-    print(boxes_list)
+    for item in os.listdir(all_xml_dir):
+        item_dir = os.path.join(all_xml_dir, item)
+        image_name, boxes_list = parse_xml(xml=item_dir)
+        # print(image_name)
+        # print(boxes_list)
