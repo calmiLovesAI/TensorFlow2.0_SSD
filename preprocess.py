3ceb3f568b39d61808aa71d26bc24ce458c2333e
calmisential <xinyu_std@163.com>
2020-01-25 09:03:10 +0800

    Resize input image

--- preprocess.py
+++ preprocess.py
@@ -2,14 +2,3 @@ import tensorflow as tf
 from configuration import IMAGE_WIDTH, IMAGE_HEIGHT, CHANNELS
 
 
-def preprocess_image(img_path):
-    # read pictures
-    img_raw = tf.io.read_file(img_path)
-    # decode pictures
-    img_tensor = tf.image.decode_jpeg(img_raw, channels=CHANNELS)
-    # resize
-    img_tensor = tf.image.resize(img_tensor, [IMAGE_HEIGHT, IMAGE_WIDTH])
-    img_tensor = tf.cast(img_tensor, tf.float32)
-    # normalization
-    img = img_tensor / 255.0
-    return img
--- utils/tools.py
+++ utils/tools.py
@@ -1,4 +1,7 @@
+import tensorflow as tf
 import numpy as np
+
+from configuration import CHANNELS, IMAGE_HEIGHT, IMAGE_WIDTH
 
 
 def x_y_meshgrid(x_row, y_col):
@@ -8,3 +11,13 @@ def x_y_meshgrid(x_row, y_col):
     X = X.flatten()
     Y = Y.flatten()
     return X, Y
+
+
+def preprocess_image(img_path):
+    # read pictures
+    img_raw = tf.io.read_file(img_path)
+    # decode pictures
+    img_tensor = tf.io.decode_image(contents=img_raw, channels=CHANNELS, dtype=tf.dtypes.float32)
+    # resize
+    img_tensor = tf.image.resize(img_tensor, [IMAGE_HEIGHT, IMAGE_WIDTH])
+    return img_tensor
